2713e251e8019919aa57cded11cca22d01fdd333
Alan Hong <xiupan@gmail.com>
2018-01-23 14:28:19 -0600

    Figured it out. It was not Travis, it was JSHint being overly strict. Properly configured now.

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-let http = require('http');
+let http = require('http')
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n');
-}).listen(1337, '127.0.0.1');
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello Travis!\n')
+}).listen(1337, '127.0.0.1')
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:1337/')

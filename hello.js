74abf92f89b2da3abbce132fa051fa4b25b086bf
Alan Hong <xiupan@gmail.com>
2018-01-23 14:00:30 -0600

    Changed some stuff.

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-let http = require('http')
+var http = require('http');
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n') // this will FAIL travis ci lint
-}).listen(1337, '127.0.0.1')
+}).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/')
+console.log('Server running at http://127.0.0.1:1337/');
